2576792ac4998364192300d799f6deb5d34c8862
Weltgeist <narcisse118@gmail.com>
2020-06-15 00:55:04 -0400

    Added inRange Function

--- _.js
+++ _.js
@@ -9,6 +9,20 @@ const _ = {
    */
   clamp(number, lower, upper){
     return Math.min(Math.max(number,lower), upper);
+  },
+  inRange(number, start, end){
+    let temp = 0;
+    let isInRange = false;
+    if (typeof end === 'undefined') {
+      end = start;
+      start = 0;
+    } else if ( start > end) {
+      temp = end;
+      end = start;
+      start = temp;
+    }
+    isInRange = number >= start && number < end;
+    return isInRange;
   }
 };
 
