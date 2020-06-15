dc3e23944d925bc4b51d31fd5a3874cdc415346c
Weltgeist <narcisse118@gmail.com>
2020-06-15 00:30:58 -0400

    Added doxy comments

--- _.js
+++ _.js
@@ -1,5 +1,12 @@
 
 const _ = {
+  /**
+   * clamps a number between two inclusive bounds.
+   * @param {num} number Number to bound
+   * @param {num} lower Lower Bound
+   * @param {num} upper Upper bound
+   * @returns {num} Bounded number
+   */
   clamp(number, lower, upper){
     return Math.min(Math.max(number,lower), upper);
   }
